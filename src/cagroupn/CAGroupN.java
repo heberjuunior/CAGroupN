a965a698c03e808e39041a46473e786a89e924d4
Heber Junior <72036949+heberjuunior@users.noreply.github.com>
2020-11-18 18:11:51 +0000

    Heber's first commit

---
+++ src/cagroupn/CAGroupN.java
@@ -0,0 +1,23 @@
+
+package cagroupn;
+
+/**
+ *
+ * @author heber
+ */
+public class CAGroupN {
+
+   
+    public static void main(String[] args) {
+       heberMethod(5,2);
+       
+    }
+    
+    static int heberMethod(int x, int y) {
+        
+        System.out.println("the sum of " + x +" and " + y + " is "+ (x+y));
+        return 0;
+    }
+    
+  
+}
