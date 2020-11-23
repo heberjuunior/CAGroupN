0d8aacd6b6b94133939746c71eb148a274f895e3
Carlos Alfonso Salinas Staarthof <csalinas1029@gmail.com>
2020-11-23 15:28:53 +0000

    Carlos First commit

--- src/cagroupn/CAGroupN.java
+++ src/cagroupn/CAGroupN.java
@@ -6,18 +6,28 @@ package cagroupn;
  * @author heber
  */
 public class CAGroupN {
-
-   
-    public static void main(String[] args) {
-       heberMethod(5,2);
-       
+    static void carlosMethod(){
+            /**
+             * 
+             * author: Carlos Salinas
+             */
+            System.out.println("Hello World");
     }
     
+           
     static int heberMethod(int x, int y) {
+        /**
+         * author: Heber Junior
+         */
         
         System.out.println("the sum of " + x +" and " + y + " is "+ (x+y));
         return 0;
     }
     
-  
+    public static void main(String[] args) {
+       heberMethod(5,2);
+       carlosMethod();
+       
+    }
+        
 }
