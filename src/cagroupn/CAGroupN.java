262d046ca1cfb0e61969db587c56442cca301fb6
Gustavo Bomfim Teixeira <gustavob.teixeira@users.noreply.github.com>
2020-11-29 21:17:07 +0000

    adding a new code to calculate the square.

--- src/cagroupn/CAGroupN.java
+++ src/cagroupn/CAGroupN.java
@@ -1,4 +1,3 @@
-
 package cagroupn;
 
 import java.util.Scanner;
@@ -8,7 +7,52 @@ import java.util.Scanner;
  * @author heber
  */
 public class CAGroupN {
+    static void carlosMethod(){
+            /**
+             * 
+             * author: Carlos Salinas
+             */
+            System.out.println("Hello World");
+    }
+    
+           
+    static int heberMethod(int x, int y) {
+        /**
+         * author: Heber Junior
+         */
+        
+        System.out.println("the sum of " + x +" and " + y + " is "+ (x+y));
+        return 0;
+    }
 
+
+
+
+    static int yuboMethod(){
+         /**
+         * author: Yubo Wang
+         */  
+
+        Scanner myKB =new Scanner(System.in);
+
+        try{
+
+            System.out.println("Please enter your first number");
+
+            int Num1 =myKB.nextInt();
+
+            System.out.println("Please enter your second number");
+
+            int Num2 =myKB.nextInt();
+
+            System.out.println("The sum of " + Num1 + "minus " + Num2 + "is " + (Num1-Num2));
+
+}catch(Exception e){
+            System.out.println("Error");
+    }
+
+    return 0;
+    }
     static int gustavoMethod() {
         /**
          * author: Gustavo Bomfim Teixeira
@@ -31,9 +75,12 @@ public class CAGroupN {
          return(square*square);
     }
     
+
     public static void main(String[] args) {
-     
+       heberMethod(5,2);
+       carlosMethod();
        gustavoMethod();
+       yuboMethod();
     }
-        
+
 }
